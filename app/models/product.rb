4ed3cee8c68299df1c386a92ab49195c1612f5ad
Alejandro Cabrera <alexperto@gmail.com>
2011-08-16 20:43:45 -0700

    add title length validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ActiveRecord::Base
   validates_format_of  :image_url,
                       :with    => %r{\.(gif|jpg|png)$}i,
                       :message => 'must be a URL for GIF, JPG or PNG image'
-
+  validates_length_of :title, :minimum => 10
 protected
   def price_must_be_at_least_a_cent
     errors.add( :price, 'should be at least 0.01' ) if price.nil? || price < 0.01
