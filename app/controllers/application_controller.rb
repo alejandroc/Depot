cf6d3988cea74f2a035121d8f2fda782aa878f50
Alejandro Cabrera <alexperto@gmail.com>
2011-08-18 23:37:57 -0700

    playtime, add current date to the sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_date
+    days = %w( Monday Tuesday Wednesday Thursday Friday Saturday Sunday )
+    months = %w( January February March April May June August September Octuber November December)
+    "#{days[ Date.today.cwday - 1 ] } - #{ months[ Date.today.month - 1 ] } #{ Date.today.day }, #{ Date.today.year}"
+  end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @current_date = current_date
     @products = Product.for_sale
   end
 
